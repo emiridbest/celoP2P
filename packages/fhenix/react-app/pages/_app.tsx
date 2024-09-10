f2348c17b3b4617a05f187ec6ad6946bd864aa11
emiridbest <udogwuemiri@gmail.com>
2024-09-10 05:16:21 +0100

    session2

--- packages/fhenix/react-app/pages/_app.tsx
+++ packages/fhenix/react-app/pages/_app.tsx
@@ -3,7 +3,7 @@ import {
   connectorsForWallets,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import { injectedWallet, metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
