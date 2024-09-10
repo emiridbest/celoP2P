dac81262e300c0d46350579312f2d4e6d0df92bd
emiridbest <udogwuemiri@gmail.com>
2024-09-10 05:39:21 +0100

    session2

--- packages/fhenix/react-app/pages/_app.tsx
+++ packages/fhenix/react-app/pages/_app.tsx
@@ -1,9 +1,3 @@
-import {
-  RainbowKitProvider,
-  connectorsForWallets,
-} from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";
-import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
@@ -151,19 +145,6 @@ function App({ Component, pageProps }: AppProps) {
 
 
 const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; // get one at https://cloud.walletconnect.com/app
-
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: "Recommended",
-      wallets: [injectedWallet],
-    },
-  ],
-   {
-      appName: "Celo Composer",
-      projectId,
-   }
-  );
 
 
 
